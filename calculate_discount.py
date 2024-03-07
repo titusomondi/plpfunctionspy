6fcb26ce7569d25d12e1ce463d0094ffafd6973b
Titus Omondi <101582094+titusomondi@users.noreply.github.com>
2024-03-07 21:56:39 +0300

    initial commit

--- calculate_discount.py
+++ calculate_discount.py
@@ -4,7 +4,7 @@ def calculate_discount(price, discount_percent):
         final_price = price - discount_amount
         return final_price
     else:
-        
+        return price
 
 original_price = float(input("Enter the original price of the item: "))
 discount_percent = float(input("Enter the discount percentage: "))
